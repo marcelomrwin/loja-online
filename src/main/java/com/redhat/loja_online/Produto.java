4e8e556a1803df539f71065da62778c78fdb5b63
rhdmAdmin <>
2019-08-23 11:57:29 -0300

    adicionando produto {/src/main/java/com/redhat/loja_online/Produto.java}

--- src/main/java/com/redhat/loja_online/Produto.java
+++ src/main/java/com/redhat/loja_online/Produto.java
@@ -6,12 +6,47 @@ package com.redhat.loja_online;
 
 public class Produto implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Produto() {
-    }
+	@org.kie.api.definition.type.Label(value = "Descrição")
+	private java.lang.String descricao;
+	@org.kie.api.definition.type.Label(value = "Valor Unitário")
+	private java.lang.Double valorUnitario;
+	@org.kie.api.definition.type.Label(value = "Código")
+	private java.lang.String codigo;
 
+	public Produto() {
+	}
 
+	public java.lang.String getDescricao() {
+		return this.descricao;
+	}
 
+	public void setDescricao(java.lang.String descricao) {
+		this.descricao = descricao;
+	}
+
+	public java.lang.Double getValorUnitario() {
+		return this.valorUnitario;
+	}
+
+	public void setValorUnitario(java.lang.Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public java.lang.String getCodigo() {
+		return this.codigo;
+	}
+
+	public void setCodigo(java.lang.String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Produto(java.lang.String descricao, java.lang.Double valorUnitario,
+			java.lang.String codigo) {
+		this.descricao = descricao;
+		this.valorUnitario = valorUnitario;
+		this.codigo = codigo;
+	}
 
 }
